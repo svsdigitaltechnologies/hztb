278af8969756c94a7325604683e7d0dfb59b7ece
Srinivas Kairamkonda <srinivas.kairamkonda@gmail.com>
2016-07-24 23:12:51 -0500

    fixed more sonar

--- hztb-servicemanager/src/main/java/com/svs/hztb/sm/ServiceManager.java
+++ hztb-servicemanager/src/main/java/com/svs/hztb/sm/ServiceManager.java
@@ -54,7 +54,7 @@ public class ServiceManager extends SpringBootServletInitializer
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ServiceManager.class);
+		return application.sources(ServiceManager.class)
 	}
 
 	@Override
